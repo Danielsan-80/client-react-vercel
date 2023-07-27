32caa681ba67bee45ded0ef04f86f914543f1ec5
Danielsan-80 <countzero80@gmail.com>
2023-07-27 11:19:04 +0200

    client-react-vercel-16

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,7 +3,7 @@ import { useSearchParams } from 'react-router-dom'
 import styles from '../modules/blog.module.css'
 import BlogCard from '../components/BlogCard'
 
-const Blog = () => {
+const Search = () => {
   const [searchParams] = useSearchParams()  
   const [posts, setPosts] = useState(null)
   const [error, setError] = useState(null)
@@ -18,13 +18,8 @@ useEffect(()=>{
       body: JSON.stringify({searchTerm: searchParams.get('term')})
     })
   
-    const data = await res.json()
-    if(!res.ok){
-      return setError(data)
-    }
-    
-    setError(null)
-    setPosts(data)
+    const posts = await res.json()
+    setPosts(posts)
   }
 
   getPosts()
@@ -37,7 +32,7 @@ useEffect(()=>{
     <h1 className={styles.title}>Search Results for: {searchParams.get('term')}</h1>
     
     <div className={styles.container}>
-        {error ? <p>{error.error}</p> : posts.map((post)=>(
+         {posts && posts.map((post)=>(
             <BlogCard key={post._id} post={post} />
         ))}
         
@@ -46,4 +41,4 @@ useEffect(()=>{
   )
 }
 
-export default Blog
+export default Search
